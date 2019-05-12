388e0ea1ed5702fd1acf032951dc909fd3329cc1
edwinbbu <edwinbbu@gmail.com>
2019-05-12 20:38:09 +0530

    show details added

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+import { Col } from 'react-bootstrap'
 class Detail extends Component {
     render() {
         const { show } = this.props.location.state.showDetails;
@@ -8,6 +8,17 @@ class Detail extends Component {
             <div className="container">
                 <h2>{show.name}</h2>
                 <div dangerouslySetInnerHTML={{ __html: show.summary }} />
+                <div className="row">
+                    <Col>
+                        <p><b>Status:</b>&nbsp;{show.status}</p>
+                        <p><b>Runtime:</b>&nbsp;{show.runtime}</p>
+                        <p><b>Schedule:</b>&nbsp;{show.schedule.days[0]}</p>
+                        <p><b>Time:</b>&nbsp;{show.schedule.time}</p>
+                    </Col>
+                    <Col>
+                        <img style={{ width: "400px", height: "300px" }} scr={show.image.original} alt="" />
+                    </Col>
+                </div>
             </div>
         )
     }
